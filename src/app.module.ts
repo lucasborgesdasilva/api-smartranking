6c2ba0e333ea0be28ef816fe113667a0358d23ae
Lucas Borges <lucas.borges@portalsolar.com.br>
2023-09-05 00:28:20 -0300

    lib do mongoose instalada, configurado a conexão com o DB no AppModule e criado Schema de Jogadores

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://Admin:ml6ztc1fkf79@cluster0.8vynpsf.mongodb.net/smartraking?retryWrites=true&w=majority',
+    ),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
