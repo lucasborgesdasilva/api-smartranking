ab43764513ee1bf9b8246feb607138f148de3f6a
Lucas Borges <lucas.borges@portalsolar.com.br>
2024-01-02 22:16:05 -0300

    Modulo de desafio criado e categorias atualizado com novo método

--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -94,4 +94,22 @@ export class CategoriasService {
 
     return categoriaEncontrada;
   }
+
+  async consultarCategoriaDoJogador(idJogador: any): Promise<Categoria> {
+    const jogadores = await this.jogadoresService.consultarTodosJogadores();
+
+    const jogadorFilter = jogadores.filter(
+      (jogador) => jogador._id == idJogador,
+    );
+
+    if (jogadorFilter.length == 0) {
+      throw new BadRequestException(`O id ${idJogador} não é um jogador!`);
+    }
+
+    return await this.categoriaModel
+      .findOne()
+      .where('jogadores')
+      .in(idJogador)
+      .exec();
+  }
 }
